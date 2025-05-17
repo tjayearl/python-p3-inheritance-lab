889c1e28ac50136e7607f95b7b8f7a126f98154a
tjayearl <iamtjayearl@gmail.com>
2025-05-17 09:46:06 +0300

    Update teacher.py

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,17 @@
-#!/usr/bin/env python
-
-from user import User
-
 import random
+from .user import User
 
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
